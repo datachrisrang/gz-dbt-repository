80b2d3d02db4fc124d3882fc441922e9826b2b27
datachrisran@gmail.com <datachrisran@gmail.com>
2024-02-06 20:00:17 +0000

    calculo de purchase_cost y margin en int_sales_margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,15 @@
+with subquery AS (
+    SELECT
+        pdt_id,
+        quantity,
+        revenue,
+        purchase_price * quantity AS purchase_cost
+        
+    FROM {{ref("stg_raw__sales")}} AS sales
+    JOIN {{ref("stg_raw__product")}} AS product
+    ON sales.pdt_id = product.products_id
+)
+select
+ *,
+ round((revenue - purchase_cost),2) as margin
+from subquery
