c9d2c4f4233d3645bd2de465a765542c4ae27bff
datachrisran@gmail.com <datachrisran@gmail.com>
2024-02-07 15:03:17 +0000

    union de todas las campañas en una tabla intermedia

--- models/staging/raw/stg_raw__adwords.sql
+++ models/staging/raw/stg_raw__adwords.sql
@@ -12,7 +12,7 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name,
+        campgn_name as campaing_name,
         cast (ads_cost as float64) as ads_cost,
         impression,
         click
