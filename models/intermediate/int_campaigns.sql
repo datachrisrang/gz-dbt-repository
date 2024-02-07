ec538904a56c4e2d6428d4eb7b123f6ea9e1ade6
datachrisran@gmail.com <datachrisran@gmail.com>
2024-02-07 19:17:24 +0000

    uso de función dbt_utils.union_relations para unir tablas campaigns

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,3 +1,8 @@
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adwords'),ref('stg_raw__bing'),ref('stg_raw__criteo'),ref('stg_raw__facebook') ] 
+) }}
+
+/*
 SELECT *
 FROM {{ref('stg_raw__adwords')}}
 UNION ALL 
@@ -9,3 +14,4 @@ FROM {{ref('stg_raw__criteo')}}
 UNION ALL 
 SELECT *
 FROM {{ref('stg_raw__facebook')}}
+*/
